caf39d21e34d5d1613c372b2542c6103e646d053
Nishant Popli <nishantpopli1234@gmail.com>
2024-07-22 00:57:23 +0530

    bugs fixes

--- src/components/Phone.tsx
+++ src/components/Phone.tsx
@@ -22,12 +22,13 @@ const Phone = ({ imgSrc, className, dark = false, ...props }: PhoneProps) => {
             : "/phone-template-white-edges.png"
         }
         className="pointer-events-none z-50 select-none"
-        alt=""
+        alt="phone image"
       />
+
       <div className="absolute -z-10 inset-0">
         <img
+          className="object-cover min-w-full min-h-full"
           src={imgSrc}
-          className="object-cover"
           alt="overlaying phone image"
         />
       </div>
